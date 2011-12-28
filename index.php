04e30acbe2efaf9006070433b181d9ca5d414771
Meghan Welford <mlwelford@gmail.com>
2011-12-27 19:47:58 -0500

    CSS & Images

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+
+<html lang="en">
+<head>
+	<!--[if lt IE 9]>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,800italic,300italic,600italic,400,300,600,700,800' 
+		rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+	<![endif]-->
+		
+	<link rel="stylesheet" type="text/css" href="assets/css/reset.css" />
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css" />
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Meghan Welford</title>
+	<meta name="generator" content="TextMate http://macromates.com/">
+	<meta name="author" content="Meghan Welford">
+	
+	<!-- Google Analytics -->
+	<script src="assets/js/google_analytics.js" type="text/javascript"></script>
+	
+	<!-- Share This Widget -->
+	<script type="text/javascript">var switchTo5x=true;</script><script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script><script type="text/javascript">stLight.options({publisher:'376f0fc1-9bad-4a27-ac42-7d790c6a7b78'});</script>
+</head>
+<body>
+	
+	<h1 class="mw">Meghan Welford</h1>
+	
+	<!-- LEFT COLUMN -->
+	<div class="left">
+		<!-- MAIN CONTENT -->
+		<div class="content" id="main">
+			<h2>Under Construction</h2>
+			<p>My portfolio is Underway!  Please be patient.  <a href="mailto:mlwelford@gmail.com">Contact me</a> if you have any questions!</p>
+		</div>
+		
+		<!-- PREVIEW -->
+		<div class="content" id="main">
+			<a href="http://css3.meghanwelford.com"><img src="assets/images/samples/border_radius.png" alt="Border Radius" width="100%"></a>
+			<h2>Preview</h2>
+			<p><a href="">Here's</a> something to keep you occupied!</p>
+		</div>
+		
+	</div>
+	
+	<!-- RIGHT COLUMN -->
+	<div class="right">
+		<div class="content">
+			<h3>About Me</h3>
+			<p>My name is Meghan Welford.  I am currently a Master's of Architecture candidate at Clemson University.</p>
+		</div>
+
+		<div class="content">
+			<h3>Skills</h3>
+			<p>Training for <span>architecture</span>.  Passion for <span>design</span>.</p>
+			<ul>
+					<li><a href="">Architecture</a></li>
+					<li><a href="">Graphics</a></li>
+					<li><a href="">Web Design & Development</a></li>
+			</ul>
+		</div>
+		
+		<div class="content" style="text-align: center;">
+				<h3 class="share">Sharing is Caring</h3>
+			<span  class='st_facebook_large' ></span>
+			<span  class='st_twitter_large' ></span>
+			<span  class='st_google_large' ></span>
+			<span  class='st_email_large' ></span>
+		</div>
+	</div>
+
+</body>
+</html>

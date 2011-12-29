614143618bdc45cd1d6b98981c34282c88245a51
Meghan Welford <mlwelford@gmail.com>
2011-12-29 10:19:58 -0500

    Update Content

--- assets/php/content/content.php
+++ assets/php/content/content.php
@@ -1,22 +1,32 @@
 <?
 	// HOME PAGE CONTENT - DISPLAY ALL
 	content($category,$sort);
-	foreach($content as $c): 
-	?>
+	if(count($content)!=0):
+		foreach($content as $c): 
+		?>
 	
-	<div class="content" id="main">
-		<? if($c['img']): ?>
-			<div class="image"><a href="item.php?cid=<? echo $c['id'];?>"><img src="assets/images/<? echo $c['img'];?>" alt="Border Radius" width="100%"></a></div>
-		<? endif; ?>
-		<h2><? echo $c['heading'];?></h2>
-		<? echo $c['content'];?>
+		<div class="content" id="main">
+			<? if($c['img']): ?>
+				<div class="image"><a href="item.php?cid=<? echo $c['id'];?>"><img src="assets/images/<? echo $c['img'];?>" 
+				alt="<? echo $c['title'];?>" width="100%"></a></div>
+			<? endif; ?>
+			<h2><? echo $c['heading'];?></h2>
+			<? echo $c['content'];?>
 		
-		<!-- IF AN IMAGE EXISTS, LINK TO THE SLIDESHOW PAGE -->
-		<? if($c['img']): ?>
-		<p><a href="item.php?cid=<? echo $c['id'];?>">Check it out &rarr;</a></p>
-		<? endif; ?>
-	</div>
+			<!-- IF AN IMAGE EXISTS, LINK TO THE SLIDESHOW PAGE -->
+			<? if($c['img']): ?>
+			<p><a href="item.php?cid=<? echo $c['id'];?>">Check it out &rarr;</a></p>
+			<? endif; ?>
+		</div>
+		<? 
+		endforeach;
 	
-	<? endforeach;?>
+	else:?>
+		<div class="content" id="main">
+			<h2 align="center">Sorry, there are no matches!</h2>
+		</div>
+	<?
+	endif;
+	?>
 
 
